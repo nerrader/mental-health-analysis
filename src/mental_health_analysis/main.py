988d49d8a5ad4f1a0a231393d84be895df8c128f
nerrader <nerrader@proton.me>
2026-06-26 19:39:50 +0700

    feat: add the first data cleaning to main.py and the first jupyter notebook

--- src/mental_health_analysis/main.py
+++ src/mental_health_analysis/main.py
@@ -1,12 +1,41 @@
 import pandas as pd
+import numpy as np
 
 from mental_health_analysis import constants as const
 
 
 def main() -> None:
+    df: pd.DataFrame = pd.read_csv(
+        const.RAW_DATA_DIR_PATH / "mental-health-in-tech-2016.csv"
+    )
+    # Dropped because it is meant to be a global analysis, not one focused on the US only.
+    # A global analysis wouldn't need this type of data.
+    df = df.drop(
+        columns=[
+            "What US state or territory do you live in?",
+            "What US state or territory do you work in?",
+        ]
+    )
+    general_value_map: dict = {
+        "1": True,
+        "0": False,
+        "yes": True,
+        "no": False,
+        "i don't know": np.nan,
+        "maybe": np.nan,
+        "n/a": np.nan,
+        "i'm not sure": np.nan,
+        "unsure": np.nan,
+    }
 
-    df = pd.read_csv(const.RAW_DATA_DIR_PATH / "mental-health-in-tech-2016.csv")
-    print(df)
+    for column_name, column_values in df.items():
+        df[column_name] = (
+            column_values.astype(str).str.lower().replace(general_value_map)
+        )
+
+    df.to_csv(
+        const.PROCESSED_DATA_DIR_PATH / "processed.csv", index=False, encoding="utf-8"
+    )
 
 
 if __name__ == "__main__":
