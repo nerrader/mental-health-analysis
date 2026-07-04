999a6270b84c12f9c6bc50473a2d5bf7d812ec76
nerrader <nerrader@proton.me>
2026-07-04 16:34:23 +0700

    feat: add more validation queries

--- queries/01_analysis_salary_vs_satisfaction_plateau.sql
+++ queries/01_analysis_salary_vs_satisfaction_plateau.sql
@@ -15,3 +15,28 @@ SELECT ROUND(salary_usd, -4) as salary_bucket, COUNT(1) as num_of_employees
 FROM `ivory-streamer-489402-u7`.mental_health_analysis.core_mental_health
 GROUP BY salary_bucket 
 ORDER BY salary_bucket;
+
+-- somehow all of these correlations are 0 so you cant use these 3 for a responsibility index
+SELECT
+	CORR(salary_usd, work_hours_per_week) as salary_hours_corr,
+	CORR(salary_usd, deadline_pressure_score) as salary_deadline_pressure_corr,
+	CORR(deadline_pressure_score, work_hours_per_week) as deadline_hours_corr,
+FROM
+	`ivory-streamer-489402-u7`.mental_health_analysis.core_mental_health;
+
+-- this will show a positive correlation, as the higher the int_seniority_level, the higher the avg salary usd
+SELECT 
+	CASE
+		WHEN LOWER(seniority_level) = 'junior' THEN 1
+		WHEN LOWER(seniority_level) = 'mid' THEN 2
+		WHEN LOWER(seniority_level) = 'senior' THEN 3
+		WHEN LOWER(seniority_level) = 'lead' THEN 4
+		WHEN LOWER(seniority_level) = 'manager' THEN 5
+		WHEN LOWER(seniority_level) = 'principal' THEN 6
+		ELSE 0
+	END AS int_seniority_level,
+	AVG(salary_usd) as salary_usd,
+FROM
+	`ivory-streamer-489402-u7`.mental_health_analysis.core_mental_health
+GROUP BY int_seniority_level
+ORDER BY int_seniority_level;
