9d8ffc423072dded3acd163c7ad4c3b5931c8a11
nerrader <nerrader@proton.me>
2026-07-05 21:41:06 +0700

    feat: add seniority level to main query

--- queries/01_analysis_salary_vs_satisfaction_plateau.sql
+++ queries/01_analysis_salary_vs_satisfaction_plateau.sql
@@ -1,4 +1,14 @@
+-- the main query to answer the first questions hypothesis
 SELECT
+	CASE
+		WHEN LOWER(seniority_level) = 'junior' THEN 1
+		WHEN LOWER(seniority_level) = 'mid' THEN 2
+		WHEN LOWER(seniority_level) = 'senior' THEN 3
+		WHEN LOWER(seniority_level) = 'lead' THEN 4
+		WHEN LOWER(seniority_level) = 'manager' THEN 5
+		WHEN LOWER(seniority_level) = 'principal' THEN 6
+		ELSE 0
+	END AS int_seniority_level, -- to test out my first hypothesis regarding more responsibility
 	ROUND(salary_usd, -4) as salary_bucket,
 	ROUND(AVG(job_satisfaction_score), 2) as avg_job_satisfaction_score,
 	ROUND(AVG(stress_score), 2) as avg_stress_score,
@@ -6,9 +16,9 @@ SELECT
 	ROUND(AVG(phq9_score), 2) as avg_phq9_score,
 	ROUND(AVG(gad7_score), 2) as avg_gad7_score,
 FROM `ivory-streamer-489402-u7`.mental_health_analysis.core_mental_health
-GROUP BY salary_bucket
+GROUP BY int_seniority_level, salary_bucket
 HAVING COUNT(1) >= 100 -- filters data if num_of_employees in that salary bracket are less than 100
-ORDER BY salary_bucket;
+ORDER BY int_seniority_level, salary_bucket;
 
 -- This was just to see how many number of employees were in each salary bucket
 SELECT ROUND(salary_usd, -4) as salary_bucket, COUNT(1) as num_of_employees
@@ -25,6 +35,7 @@ FROM
 	`ivory-streamer-489402-u7`.mental_health_analysis.core_mental_health;
 
 -- this will show a positive correlation, as the higher the int_seniority_level, the higher the avg salary usd
+-- also since a higher salary usually means higher responsibility, this kinda also validates the data sorta
 SELECT 
 	CASE
 		WHEN LOWER(seniority_level) = 'junior' THEN 1
@@ -35,7 +46,7 @@ SELECT
 		WHEN LOWER(seniority_level) = 'principal' THEN 6
 		ELSE 0
 	END AS int_seniority_level,
-	AVG(salary_usd) as salary_usd,
+	AVG(salary_usd) as avg_salary_usd,
 FROM
 	`ivory-streamer-489402-u7`.mental_health_analysis.core_mental_health
 GROUP BY int_seniority_level
